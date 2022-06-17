b2c380f099c8fb6c2a3fbe63dae34e716e8d7f54
Fab0504 <181870184@smail.nju.edu.cn>
2022-06-17 10:03:51 +0800

    update

--- README.md
+++ README.md
@@ -1,64 +1,19 @@
-<p align="center"> <img src="figure/title.jpg" width=800 align="center"/> </p>
-
-<p align="center">
-
-  <img alt="GitHub" src="https://img.shields.io/github/license/EricZhu-42/PedagogySquare_Downloader">
-  <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/EricZhu-42/PedagogySquare_Downloader">	
-  <img alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/EricZhu-42/PedagogySquare_Downloader">
-  <img alt="GitHub code size in bytes" src="https://img.shields.io/github/languages/code-size/EricZhu-42/PedagogySquare_Downloader">
-  <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/EricZhu-42/PedagogySquare_Downloader">
-  </br>
-  <img alt="GitHub stars" src="https://img.shields.io/github/stars/EricZhu-42/PedagogySquare_Downloader">
-  <img alt="GitHub All Releases" src="https://img.shields.io/github/downloads/EricZhu-42/PedagogySquare_Downloader/total">
-  <img alt="GitHub issues" src="https://img.shields.io/github/issues-raw/EricZhu-42/PedagogySquare_Downloader">
-  <img alt="GitHub closed issues" src="https://img.shields.io/github/issues-closed-raw/EricZhu-42/PedagogySquare_Downloader">
-  <img alt="PRs welcome" src="https://img.shields.io/badge/PRs-welcome-brightgreen">
-
-</p>
-
 # 教学立方课件下载器
 
 在线教学平台——[教学立方](https://teaching.applysquare.com)的课件批量下载脚本，基于**Python** + **Requests**
 
 > 创建日期：2020-03-30  
-> 更新日期：2022-03-06
+> 更新日期：2020-09-14
 
-## 下载地址
+## 版本更新信息（重要！）
 
-**更新日期：2021年6月21日**
+1. 已于9月14日晚发布最新版本的release包，请于9月14日前获取代码的同学重新下载脚本。
 
-推荐通过Github release[下载](https://github.com/EricZhu-42/PedagogySquare_Downloader/releases/download/v1.7/PedagogySquare_Downloader_20210621.zip)；若连接速度较慢，亦可通过国内镜像地址[下载](https://box.nju.edu.cn/f/4214a2459d4347df9394/?dl=1)。
+2. 已**修复教学立方版本更新导致无法正常下载课件的问题**，且进行了稳定性优化。
 
+3. 当前版本的脚本**解除了先前版本对ChromeDriver与Selenium的依赖**，目前**仅需安装Requests即可正常使用**。
 
-## 版本更新日志
-
-### 2021年6月21日：功能性更新
-
-在 `config.json` 文件中增加了 `cid_include_list` 项，在非空时将仅下载指定的部分课程。
-
-### 2021年4月26日：功能性更新
-
-在 `config.json` 文件中增加了 `keep_dirs` 项，可选择下载时是否保持课件的文件夹结构（默认为 `false`）
-
-### 2021年3月5日：功能性更新
-
-为了方便课件管理，当前版本的 `config.json` 文件中增加了 `save_path` 项（支持绝对路径/相对路径），可设置下载文件的保存目录。
-
-请参照下文“配置文件说明”一节，调整下载目录（默认为空，表示下载到脚本的相同目录）
-
-### 2021年3月1日：Windows平台下的工具封装
-
-**TLDR: 当前版本做了脚本封装，现在Windows平台下不安装Python环境也能用了**
-
-自2021年3月1日起，我们将在release中为**Windows平台**用户提供封装后的**独立可执行程序**（standalone executable）。**无需安装配置Python环境即可使用封装后的课件下载工具**，具体的使用方法为：
-
-1. 下载并解压最新的release文件包，并进入解压后的文件夹
-3. 参照下文“最简配置方案”一节，修改文件 `config.json` ，填入用户名、密码等信息
-4. 双击执行 `run.bat`，等待课件下载完成
-
-此外，新版本脚本优化了 `config.json` 的配置逻辑，推荐参考下文“配置文件说明”一节重新进行配置。
-
-> 如果更新后的脚本无法正常使用，请尝试回退至[旧版本](https://github.com/EricZhu-42/PedagogySquare_Downloader/releases/download/v1.4_stable/PedagogySquare_Downloader_20200914.zip)，观察问题是否解决，并联系开发者反馈问题，感谢！
+   > 如果更新后的脚本无法正常使用，请尝试回退至[旧版本](https://github.com/EricZhu-42/PedagogySquare_Downloader/releases/download/v1.3_stable/PedagogySquare_Downloader_20200702.zip)，观察问题是否解决，并联系开发者反馈问题，感谢！
 
 ## 程序特色
 
@@ -66,11 +21,15 @@
 2. **可下载未直接开放下载的课件**，视频等内容
 3. **可深度配置**的课程筛选/文件拓展名筛选功能
 
+以下为运行过程中控制台截图示例：
+
+![](figure/1.png)
+
 > 本程序旨在方便学生下载教学立方平台上的课件及相关教学资料，消除下载文件的重复劳动  
 > 请尊重教师的知识产权与劳动成果。除非获得教师许可，请勿将下载得到的文件在互联网上进行传播  
 > 如本程序损害了您的权益，请联系作者删除相关代码  
 
-## 运行环境
+## 开发环境
 
 开发过程中使用的环境与第三方模块版本如下：
 
@@ -78,9 +37,8 @@
 
 - **Requests** = 2.22.0
 
-为了正常运行脚本，请安装**不低于**以上版本的Python与Requests
 
-> 另：经测试，**该脚本可在移动端的终端模拟器中正常运行**；安卓平台执行Python脚本可以参考[@OrangeX4](https://github.com/OrangeX4)提供的[工具与教程](https://orangex.orangex4.cool/)。
+> **理论上**该脚本**兼容**其他版本的Python环境（新的一般没问题，旧一点的应该也行），但请在发生错误时关注兼容性问题
 
 ## 使用方法
 
@@ -88,15 +46,15 @@
 
 1. 安装对应版本的Python
 
-2. 安装对应版本的Python模块：**Requests**（推荐使用[Anaconda](https://www.anaconda.com/)进行管理）
+2. 安装对应版本的Python模块：Requests（推荐使用[Anaconda](https://www.anaconda.com/)进行管理）
 
 
 ### 2. 修改配置文件
 
 修改文件 `config.json` ，填入用户名、密码等信息
 
-> 关于如何修改配置文件，请参考“最简配置方案”一章  
-> 关于配置文件内各项参数的说明，请参考“配置文件说明”一章  
+> 关于如何修改配置文件，请参考”最简配置方案“一章  
+> 关于配置文件内各项参数的说明，请参考”配置文件说明“一章  
 
 ### 3. 运行脚本
 
@@ -119,15 +77,15 @@
 {
 	"username": "your_username",
 	"password": "your_password",
+	"download_all_ext": true,
+	"download_all_courses": true,
+	"ext_list": [],
 	"ext_expel_list": [],
-	"cid_expel_list": [],
-	"cid_include_list": [],
-        "save_path": "",
-	"keep_dirs": false
+	"cid_list": []
 }
 ```
 
-将 `your_username` 与 `your_password` （**注意保留外部的双引号**）替换成你的**手机号**和**教学立方登录密码**即可，其他参数无需修改。
+将 `your_username` 与 `your_password` （**保留双引号**）替换成你的**手机号**和**教学立方登录密码**即可，其他参数无需修改。
 
 > 请确保json文件格式正确，可参考提供的 `config_example.json` 进行配置。
 
@@ -139,20 +97,22 @@
 | -------------------- | ---- | --------------------------------------------------- |
 | username             | str  | 教学立方登录用户名（一般为手机号）                  |
 | password             | str  | 教学立方登录密码                                    |
-| ext_expel_list       | list | 排除文件的类型列表                                      |
-| cid_expel_list             | list | 排除课程的课程ID列表                                    |
-| cid_include_list | list | 保留课程的ID列表 |
-| save_path | str | 下载目录（支持绝对路径或相对路径） |
-| keep_dirs | bool | 是否保留文件的目录结构 |
+| download_all_ext     | bool | 是否下载所有类型的文件                              |
+| download_all_courses | bool | 是否下载所有课程的课件                              |
+| ext_list             | list | 下载文件的类型（如：pdf，docx，zip）                |
+| ext_expel_list       | list | 排除文件的类型                                      |
+| cid_list             | list | 需要下载的课程ID                                    |
 
-### 注意事项与说明：
+#### 注意：
 
-1. 例如：若不需要下载课程ID为 `12345` 的课程，且不需要下载 `pdf` 文件与 `mp4` 文件，可设置为：
-   - `ext_expel_list = ["pdf", "mp4"]`
-   - `cid_expel_list = [12345]`
-2. 在修改下载目录 `save_path` 时，请先在对应的位置**创建文件夹**；如无法找到对应的文件夹，将会报错并在默认目录（程序根目录）保存下载得到的文件。
-4. 课程ID在课程主页地址中查看，例如：  
-   ![](./figure/0.png)  
+1. 文件类型参数优先级为：`ext_expel_list` > `download_all_ext` > `ext_list`  
+   如：若希望下载“除了zip格式文件外的所有类型文件“，应设置参数为
+
+   - `download_all_ext` = `true`
+   - `ext_expel_list` = `["zip"]`
+
+2. 课程ID在课程主页地址中查看，例如：  
+   ![](figure/0.png)  
    图中对应课程的ID为**8261**  
 
 
--- download.py
+++ download.py
@@ -1,47 +1,36 @@
-# -*- coding: UTF-8 -*-
-"""
-@FileName: download.py
-
-@Author：zhuxinhao00@gmail.com
-
-@Create date: 2020/03/31
-
-@Modified date: 2021/09/14
-
-@description: A script to download file automatically from teaching.applysquare.com
-"""
-
 import hashlib
 import json
-import logging
 import os
-import pathlib
-import re
 import time
+import requests
 from contextlib import closing
 
-import requests
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+login_url = r"https://teaching.applysquare.com/Api/User/ajaxLogin"
+attachment_url_fmt = r'https://teaching.applysquare.com/Api/CourseAttachment/getList/token/{}?parent_id={}&page={}&plan_id=-1&uid={}&cid={}'
+course_info_url_fmt = r'https://teaching.applysquare.com/Api/Public/getIndexCourseList/token/{}?type=1&usertype=1&uid={}'
+attachment_detail_url_fmt = r'https://teaching.applysquare.com/Api/CourseAttachment/ajaxGetInfo/token/{}?id={}&uid={}&cid={}'
 
 
 # Get Hex-md5 encoded password
-def hex_md5_stringify(raw_str:str):
+def hex_md5_stringify(raw_str: str):
     md5_encoder = hashlib.md5()
     md5_encoder.update(str(raw_str).encode('utf-8'))
     return md5_encoder.hexdigest()
 
+
 # Function dealing with illegal characters of windows filename
-def filename_filter(name:str):
-    illegal_list = list('/\:*?”"<>|')
+def filename_filter(name: str):
+    illegal_list = list(r'/\:*?”"<>|')
     for char in illegal_list:
-        name = name.replace(char, ' ')
+        name = name.replace(char, '_')
     return name
 
-def construct_attchment_list(sess, token, pid, uid, cid, parent_dir):
+
+def construct_attachment_list(sess, token, pid, uid, cid):
     attachment_list = list()
     attachment_info_url = attachment_url_fmt.format(token, pid, 1, uid, cid)
-    r = sess.get(attachment_info_url, verify=False)
+    r = sess.get(attachment_info_url)
     info = r.json()['message']
     file_num = info.get('count')
 
@@ -49,183 +38,160 @@ def construct_attchment_list(sess, token, pid, uid, cid, parent_dir):
     # Add attachment path to attachment_list
     while len(attachment_list) < file_num:
         current_url = attachment_url_fmt.format(token, pid, current_page, uid, cid)
-        r = sess.get(current_url, verify=False)
+        r = sess.get(current_url)
         info = r.json()['message']
         attachment_list.extend(info.get('list'))
         current_page += 1
-    for entry in attachment_list:
-        entry["parent_dir"] = parent_dir
     return attachment_list
 
-# Load config from config.json
-with open('config.json', 'r', encoding='utf-8') as f:
-    config = json.loads(f.read())
-    user_name = config.get('username')
-    user_passwd = config.get('password')
-    ext_expel_list = config.get('ext_expel_list')
-    cid_include_list = list(map(str, config.get('cid_include_list', [])))
-    cid_expel_list = list(map(str, config.get('cid_expel_list', [])))
-    save_path = config.get('save_path', "")
-    keep_dirs = config.get('keep_dirs', False)
+def main():
+    # Load config from config.json
+    with open('config.json', 'r') as f:
+        config = json.loads(f.read())
+        user_name = config.get('username')
+        user_passwd = config.get('password')
 
-if save_path:
-    try:
-        os.chdir(save_path)
-    except Exception as e:
-        print('Changing save_path failed for reason \"{}\", using default path instead.'.format(e))
-        time.sleep(1)
+        download_all_cid = config.get('download_all_cid')
+        cid_certain_list = config.get('cid_certain_list')
+        cid_expel_list = config.get('cid_expel_list')
 
-print("Files will be saved to ", os.getcwd())
+        download_all_ext = config.get('download_all_ext')
+        ext_certain_list = config.get('ext_certain_list')
+        ext_expel_list = config.get('ext_expel_list')
 
-# Some metadata
-login_url = r'https://teaching.applysquare.com/Api/User/ajaxLogin'
-attachment_url_fmt = r'https://teaching.applysquare.com/Api/CourseAttachment/getList/token/{}?parent_id={}&page={}&plan_id=-1&uid={}&cid={}'
-course_info_url_fmt = r'https://teaching.applysquare.com/Api/Public/getIndexCourseList/token/{}?type=1&usertype=1&uid={}'
-attachment_detail_url_fmt = r'https://teaching.applysquare.com/Api/CourseAttachment/ajaxGetInfo/token/{}?id={}&uid={}&cid={}'
+        download_all_filename = config.get('download_all_filename')
+        filename_certain_list = config.get('filename_certain_list')
+        filename_expel_list = config.get('filename_expel_list')
 
-# Init Requests session
-sess = requests.Session()
+    sess = requests.Session()
 
-# Login in
-print("Trying to log in, please wait ...")
-login_request = sess.post(login_url, data={"email" : user_name, "password" : hex_md5_stringify(user_passwd)}, verify=False)
-
-login_response = login_request.json()
-login_info = login_response['message']
-
-try:
+    # login
+    print("login, wait ...")
+    login_request = sess.post(login_url, data={"email": user_name, "password": hex_md5_stringify(user_passwd)})
+    login_response = login_request.json()
+    login_info = login_response['message']
     token = login_info['token']
-except TypeError:
-    print("Login Failed, please check your username & password")
-    print("Login info received: {}".format(login_info))
-    exit()
+    uid = login_info['uid']
+    print("login successfully")
 
-uid = login_info['uid']
-print("Login successfully!")
+    cid2name_dict = dict()  # cid2name_dict = {cid: course_name}
+    course_info_url = course_info_url_fmt.format(token, uid)
+    r = sess.get(course_info_url)
+    info = r.json()["message"]
 
-cid2name_dict = dict()
-course_info_url = course_info_url_fmt.format(token, uid)
-r = sess.get(course_info_url, verify=False)
-info = r.json()["message"]
-for entry in info:
-    cid2name_dict[entry.get('cid')] = entry.get('name')
+    for entry in info:
+        cid2name_dict[entry.get('cid')] = entry.get('name')
 
-cid_list = cid2name_dict.keys()
+    if download_all_cid:
+        cid_download_list = cid2name_dict.keys()
+    elif len(cid_certain_list):
+        cid_download_list = [str(x) for x in cid_certain_list]
+        for cid in cid_certain_list:
+            if not str(cid) in cid2name_dict.keys():
+                print(f"can't find course with cid {cid}")
+                cid_download_list.remove(str(cid))
+    elif len(cid_expel_list):
+        cid_download_list = cid2name_dict.keys()
+        for cid in cid_expel_list:
+            if str(cid) in cid_download_list:
+                cid_download_list.remove(str(cid))
+    else:
+        cid_download_list = list()
 
-def check_cid(cid):
-    if len(cid_include_list) and cid not in cid_include_list:
-        return False
-    return cid not in cid_expel_list
-
-print("\nReady to download the following courses:")
-for cid, cname in cid2name_dict.items():
-    if not check_cid(cid):
-        continue
-    print("Course: {:8s}, CID={:6}".format(cname, cid))
-
-for cid in cid_list:
-    cid = str(cid) # Prevent bug caused by wrong type of cid
-
-    if not check_cid(cid):
-        continue
-
-    try:
+    cwd = os.getcwd()
+    download_dir = "D:/download_backup/pedagogysquare_download"
+    for cid in cid_download_list:
         course_name = filename_filter(cid2name_dict[cid])
-    except KeyError:
-        print("Can't find course name for cid {}, maybe it's a legacy course?".format(cid))
-        course_name = "CID_{}".format(cid)
-    print("\nDownloading files of course {}".format(course_name))
+        print("downloading files of course {}".format(course_name))
 
-    # Create dir for this course
-    root= pathlib.Path(os.getcwd()) / course_name
-    if not root.exists() or root.is_file():
-        os.makedirs(root)
+        # create dir -> enter dir
+        download_course_dir = os.path.join(download_dir, course_name)
+        try:
+            os.chdir(download_course_dir)
+        except FileNotFoundError:
+            os.mkdir(download_course_dir)
+            os.chdir(download_course_dir)
 
-    # Construct attachment list, with some dirs in it
-    course_attachment_list = construct_attchment_list(sess=sess, token=token, pid=0, uid=uid, cid=cid, parent_dir=pathlib.Path("."))
+        course_attachment_list = construct_attachment_list(sess=sess, token=token, pid=0, uid=uid, cid=cid)
 
-    # Iteratively add files in dirs to global attachment list
-    dir_counter = 0
-    for entry in course_attachment_list:
-        if (entry.get('ext') == 'dir'):
-            dir_counter += 1
-            # Add dir content to attachment list
-            dir_id = entry.get('id')
-            dir_name = filename_filter(entry.get('title')) if keep_dirs else ''
-            parent_dir = entry.get('parent_dir')
-            if not (root/parent_dir/dir_name).exists():
-                os.makedirs(root/parent_dir/dir_name)
-            course_attachment_list.extend(construct_attchment_list(sess=sess, token=token, pid=dir_id, uid=uid, cid=cid, parent_dir=parent_dir/dir_name))
+        # Iteratively add files in dirs to global attachment list
+        dir_counter = 0
+        for entry in course_attachment_list:
+            if entry.get('ext') == 'dir':
+                dir_counter += 1
+                dir_id = entry.get('id')
+                course_attachment_list.extend(
+                    construct_attachment_list(sess=sess, token=token, pid=dir_id, uid=uid, cid=cid))
 
-    print("Get {:d} files with {:d} dirs".format(len(course_attachment_list)-dir_counter, dir_counter))
+        # num_files, num_dirs
+        file_counter = len(course_attachment_list) - dir_counter
+        print(f"get {file_counter} files, with {dir_counter} dirs")
 
-    # Download attachments
-    for entry in course_attachment_list:
-        ext = entry.get('ext')
-        if (ext == 'dir') or (ext in ext_expel_list):
-            continue
+        for entry in course_attachment_list:
+            # get ext and filename
+            ext = entry.get('ext')
+            if ext in entry.get('title'):
+                filename = filename_filter(entry.get('title'))
+            else:
+                filename = filename_filter("{}.{}".format(entry.get('title'), ext))
 
-        if (ext in entry.get('title')):
-            filename = filename_filter(entry.get('title'))
-        else:
-            filename = filename_filter("{}.{}".format(entry.get('title'), ext))
-        filepath = root/entry.get("parent_dir")/filename
-
-        filesize = entry.get('size')
-
-        # Get download url for un-downloadable files
-        if (entry.get('can_download') == '0'):
-            attachment_detail_url = attachment_detail_url_fmt.format(token, entry.get('id'), uid, cid)
-            r = sess.get(attachment_detail_url, verify=False)
-            info = r.json()['message']
-            entry['path'] = info.get('path')
-
-        with closing(requests.get(entry.get('path').replace('amp;', ''), stream=True)) as res:
-
-            try:
-                content_size = eval(res.headers['content-length'])
-            except Exception:
-                print("Failed to get content length of file {}, please download it manually.".format(filename))
+            if ext == 'dir':
                 continue
 
-            if filepath.exists() and filepath.is_file():
-                # If file is up-to date, continue; else, delete and re-download
-                if os.path.getsize(filepath) == content_size:
-                    print("File {:\u3000<20} is up-to-date".format(filename))
+            if not download_all_ext:
+                if ext not in ext_certain_list or ext in ext_expel_list:
                     continue
-                else:
-                    print("Updating File {}".format(filename))
-                    os.remove(filepath)
 
-            print("Downloading {:\u3000<20s}, filesize = {}".format(filename, filesize))
-            chunk_size = min(content_size, 10240)
-            with open(filepath, "wb") as f:
-                chunk_count = 0
-                start_time = time.time()
-                # previous_time = time.time()
-                # lag_counter = 0
-                total = content_size / 1024 / 1024
-                for data in res.iter_content(chunk_size=chunk_size):
-                    chunk_count += 1
-                    processed = len(data) * chunk_count / 1024 / 1024
-                    current_time = time.time()
-                    if chunk_count < 5:
-                        print(r"    Total: {:.2f} MB  Processed: {:.2f} MB ({:.2f}%)".format(total, processed, processed/total*100), end = '\r')
+            if not download_all_filename:
+                if any([x for x in filename_expel_list if x in filename]) or \
+                        not any([x for x in filename_certain_list if x in filename]):
+                    continue
+
+            filesize = entry.get('size')
+
+            # Get download url for un-downloadable files
+            if entry.get('can_download') == '0':
+                attachment_detail_url = attachment_detail_url_fmt.format(token, entry.get('id'), uid, cid)
+                r = sess.get(attachment_detail_url)
+                info = r.json()['message']
+                entry['path'] = info.get('path')
+
+            with closing(requests.get(entry.get('path').replace('amp;', ''), stream=True)) as res:
+
+                try:
+                    content_size = eval(res.headers['content-length'])
+                except Exception:
+                    print("Failed to get content length of file {}, please download it manually.".format(filename))
+                    continue
+
+                if filename in os.listdir():
+                    if os.path.getsize(filename) == content_size:
+                        print("File \"{}\" is up-to-date".format(filename))
+                        continue
                     else:
-                        remaining = (current_time-start_time)/processed*(total-processed)
-                        print(r"    Total: {:.2f} MB  Processed: {:.2f} MB ({:.2f}%), ETA {:.2f}s".format(total, processed, processed/total*100, remaining), end = '\r')
-                    f.write(data)
+                        print("Updating File {}".format(filename))
+                        os.remove(filename)
 
-                    # speed = chunk_size / 1.0 * (current_time - previous_time)
-                    # if speed < speed_threshold:
-                    #     lag_counter += 1
-                    # else:
-                    #     lag_counter = 0
+                print("Downloading {}, filesize = {}".format(filename, filesize))
+                chunk_size = min(content_size, 10240)
+                with open(filename, "wb") as f:
+                    chunk_count = 0
+                    start_time = time.time()
+                    total = content_size / 1024 / 1024
+                    for data in res.iter_content(chunk_size=chunk_size):
+                        chunk_count += 1
+                        processed = len(data) * chunk_count / 1024 / 1024
+                        current_time = time.time()
+                        if chunk_count < 5:
+                            print(r"Total: {:.2f} MB  Processed: {:.2f} MB ({:.2f}%)".
+                                  format(total, processed, processed / total * 100), end='\r')
+                        else:
+                            remaining = (current_time - start_time) / processed * (total - processed)
+                            print(r"Total: {:.2f} MB  Processed: {:.2f} MB ({:.2f}%), ETA {:.2f}s".
+                                  format(total, processed, processed / total * 100, remaining), end='\r')
+                        f.write(data)
+        os.chdir(cwd)
+    print("done")
 
-                    # if lag_counter > 10:
-                    #     print("Restart downloading of file {}".format(filename))
-                    #     attachment_list.append(entry)
-                    #     continue
-
-
-print("Done!")
+if __name__=="__main__":
+    main()
